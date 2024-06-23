0c6df783e0facb94654b55e26d09db346e3dbbb7
tnpdz123 <thanaphatgub@gmail.com>
2024-06-23 14:34:07 +0700

    a

--- src/app/_components/Profile.tsx
+++ src/app/_components/Profile.tsx
@@ -8,7 +8,7 @@ const Profile = () => {
     <div className="bg-blue-100 h-full p-4 rounded-xl px-4">
       <div className="flex justify-center items-center pt-4 pb-2">
         <Image
-          src="/a.jpg"
+          src="/tenor.gif"
           width={500}
           height={500}
           alt="profile picture"

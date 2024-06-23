eb30c4451f3ee593f83c737e16e7db2419694d03
tnpdz123 <thanaphatgub@gmail.com>
2024-06-23 14:21:41 +0700

    Update Bio.tsx

--- src/app/_components/Bio.tsx
+++ src/app/_components/Bio.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import Card from "./common/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faBrain, faUsers } from "@fortawesome/free-solid-svg-icons";
 
-type Props = {};
-
-const Bio = (props: Props) => {
+const Bio = () => {
   return (
     <Card title="Bio">
-      <div>
-An information technology engineer passionate about IoT with over 5 years of experience in developing and designing IoT systems applicable in various industries,
- from smart homes to city infrastructure management.
-  Interested in integrating AI and data analytics with IoT to create intelligent and sustainable solutions.
-   Enjoys researching and experimenting with new technologies and believes that the interconnection of devices will continuously improve our way of life. 
-   Ready to collaborate and share knowledge within the IoT community both locally and internationally.
+      <div className="flex items-center">
+       
+        <div>
+          <p className="mb-4">
+            An information technology engineer passionate about IoT with over 5 years of experience in developing and designing IoT systems applicable in various industries,
+            from smart homes to city infrastructure management.
+          </p>
+          <p className="mb-4">
+            Interested in integrating AI and data analytics with IoT to create intelligent and sustainable solutions.
+          </p>
+          <p className="mb-4">
+            Enjoys researching and experimenting with new technologies and believes that the interconnection of devices will continuously improve our way of life.
+          </p>
+          <p>
+            Ready to collaborate and share knowledge within the IoT community both locally and internationally.
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-end mt-4">
+        <FontAwesomeIcon icon={faBrain} className="text-blue-500 text-2xl mx-2" />
+        <FontAwesomeIcon icon={faUsers} className="text-blue-500 text-2xl mx-2" />
       </div>
     </Card>
   );

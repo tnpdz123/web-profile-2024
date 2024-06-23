56ba1fd7a3e58f914c82252b6bbd48e2670e62c5
tnpdz123 <thanaphatgub@gmail.com>
2024-06-23 14:25:08 +0700

    Update Goals.tsx

--- src/app/_components/Goals.tsx
+++ src/app/_components/Goals.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Card from "./common/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe, faCode, faMoneyBillAlt, faComments, faUtensils } from "@fortawesome/free-solid-svg-icons";
 
 type Props = {};
 
@@ -8,11 +10,26 @@ const Goals = (props: Props) => {
     <Card title="Goals">
       <div>
         <ul className="list-inside list-disc">
-          <li>Travel around the world</li>
-          <li>Learn a new coding skill</li>
-          <li>Have a lot of money saved</li>
-          <li>Improve communication skills</li>
-          <li>Eat every thing not a pork</li>
+          <li>
+            <FontAwesomeIcon icon={faGlobe} className="text-blue-500 mr-2" />
+            Travel around the world
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCode} className="text-purple-500 mr-2" />
+            Learn a new coding skill
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faMoneyBillAlt} className="text-green-500 mr-2" />
+            Have a lot of money saved
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faComments} className="text-yellow-500 mr-2" />
+            Improve communication skills
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faUtensils} className="text-red-500 mr-2" />
+            Eat everything (except pork)
+          </li>
         </ul>
       </div>
     </Card>
